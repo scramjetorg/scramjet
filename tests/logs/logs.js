ce1301473883d97b4641feb6ad53d0d988304142
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2016-08-22 12:40:42 +0200

    complicated example done

--- tests/logs/logs.js
+++ tests/logs/logs.js
@@ -3,8 +3,27 @@
 
 var argv = require('yargs')
     .usage('Usage: $0 [options] [source1] [source2] ...')
-    .example('$0 count -f foo.js', 'count the lines in the given file')
     .demand(1)
+    .options("origin", {
+        alias: "o",
+        array: true,
+        describe: "filter by log origin(s)"
+    })
+    .options("level", {
+        alias: "l",
+        array: true,
+        describe: "filter by log level(s)"
+    })
+    .options("source", {
+        alias: "s",
+        array: true,
+        describe: "filter by log source(s)"
+    })
+    .options("range", {
+        alias: "r",
+        array: true,
+        describe: "date between"
+    })
     .help('h')
     .alias('h', 'help')
     .epilog('Copyright 2016 Signicode')
@@ -12,7 +31,35 @@ var argv = require('yargs')
 
 const fs = require("fs");
 const path = require("path");
-const ArrayWithSortedSet = require("../../lib/util/array-with-sorted-set");
+
+const range = argv.range.map(
+    (r) => range.split(/:/).map(
+        (ts, i) => {
+            if (!ts.match(/^\d/)) {
+                return new Date(+ts);
+            } else if (ts) {
+                return new Date(ts);
+            } else {
+                return (i - 0.5) * Infinity;
+            }
+        }
+    )
+);
+const filters = [
+    argv.origin && argv.origin.length && argv.origin.length === 1 ? (item) => argv.origin[0] === item.origin : (item) => argv.origin.indexOf(item.origin) >= 0,
+    argv.source && argv.source.length && argv.source.length === 1 ? (item) => argv.source[0] === item.source : (item) => argv.source.indexOf(item.source) >= 0,
+    argv.level && argv.level.length && argv.level.length === 1 ? (item) => argv.level[0] === item.level : (item) => argv.level.indexOf(item.level) >= 0,
+    range && range.length && (item) => {
+        for (const r of range) {
+            if (item.ts > r[0] && item.ts < r[1])
+                return true;
+        }
+        return false;
+    }
+].filter((f) => f).reduce(
+    (o, f) => (i) => f(i) && o(i),
+    () => true
+);
 
 Promise.all(argv._.map(
     (source) => new Promise((solve, ject) => {
@@ -30,26 +77,31 @@ Promise.all(argv._.map(
         stream.on("error", out);
     })
 )).then(
-    (openStreams) => {
-        return new MergedStream(openStreams.map(
-            (stream) => stream.pipe(new StringStream("utf-8"))
-                .split(/[\n^$](?=\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3,}\s)/g)
-                .parse((logline) => {
-                    const ret = {};
-                    log.replace(/^([\s\d\-+\:,]+)\s+\[([^\]]+)\]\s+([^\s]+)\s+([^\-]+)\s+-\s*([^]*)$/m,
-                        (all, ts, origin, level, source, message) => {
-                            ret.ts = new Date(ts.replace(/,\d+$/, ".$1Z"));
-                            ret.origin = origin;
-                            ret.level = level;
-                            ret.source = source;
-                            ret.message = message;
-                        }
-                    );
-                    return ret;
-                })
-        )).mux(
-            (a, b) => a.ts - b.ts,
-            (sorted, streams, callback) => callback(sorted.shift())
+    (openStreams) => new MultiStream(openStreams.map(
+        (stream) => stream.pipe(new StringStream("utf-8"))
+            .split(/[\n^$](?=\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3,}\s)/g)
+            .parse((logline) => {
+                const ret = {};
+                log.replace(/^([\s\d\-+\:,]+)\s+\[([^\]]+)\]\s+([^\s]+)\s+([^\-]+)\s+-\s*([^]*)$/m,
+                    (all, ts, origin, level, source, message) => {
+                        ret.ts = new Date(ts.replace(/,\d+$/, ".$1Z"));
+                        ret.origin = origin;
+                        ret.level = level;
+                        ret.source = source;
+                        ret.message = message;
+                    }
+                );
+                return ret;
+            })
+        ))
+).then(
+    (streams) => streams.each(
+        (stream) => stream.filter(filters)
+    )
+).then(
+    (streams) => {
+        return new MergedStream(streams).mux(
+            (a, b) => a.ts - b.ts
         );
     }
 );
