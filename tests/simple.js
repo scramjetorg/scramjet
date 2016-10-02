2545296191f51a10c5d1a1726cc15495f350df43
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2016-10-02 22:23:31 +0200

    simple csv parser in 5 lines example

--- tests/simple.js
+++ tests/simple.js
@@ -1,33 +1,15 @@
 const request = require("request");
-const Transform = require("stream").Transform;
+const StringStream = require("../lib/string-stream");
 
-class SplitStreamBySeparator extends Transform {
-
-    constructor(separator, ...args) {
-        super(...args);
-        this.buffer = "";
-        this.separator = separator;
-    }
-
-    _transform(chunk, encoding, callback) {
-        this.buffer += chunk.toString("utf-8");
-        const newChunks = this.buffer.split(this.separator);
-        while(newChunks.length > 1) {
-            this.push(newChunks.shift());
-        }
-        this.buffer = newChunks[0];
-        callback();
-    }
-
-    _flush(callback) {
-        callback(null, this.buffer);
-    }
-
-}
-
+let columns = null;
 request.get("http://www.wroclaw.pl/open-data/opendata/its/parkingi/parkingi.csv")
-    .pipe(
-        new SplitStreamBySeparator(/\r?\n/g)
-    ).on("data",
-        (data) => console.log("Dat: " + data)
-    );
+    .pipe(new StringStream())
+    .split("\n")
+    .parse((line) => line.split(";"))
+    .filter((data) => columns === null ? (columns = data, false) : true) // some kind of a "pop function maybe?"
+    .map((data) => columns.reduce(
+        (acc, id, i) => (acc[id] = data[i], acc),
+        {}
+    ))
+    .on("data", console.log.bind(console))
+;
