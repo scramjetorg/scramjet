7cf0bf5753257fb36acbf2332802957b1a3fb5fe
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2016-10-26 22:50:53 +0200

    Simplyfy map/reduce/filter, fixes #4

--- samples/tests/simple.js
+++ samples/tests/simple.js
@@ -1,15 +1,31 @@
 const request = require("request");
 const StringStream = require("../../").StringStream;
 
+const hourglass = "-\\|/-";
+let i = 0;
+
 let columns = null;
-request.get("http://www.wroclaw.pl/open-data/opendata/its/parkingi/parkingi.csv")
+
+console.log();
+
+const res = request.get("http://www.wroclaw.pl/open-data/opendata/its/parkingi/parkingi.csv")
     .pipe(new StringStream())
     .split("\n")
     .parse((line) => line.split(";"))
-    .filter((data) => columns === null ? (columns = data, false) : true) // some kind of a "pop function maybe?"
+    .filter((data) => columns === null ? (columns = data, false) : true)
     .map((data) => columns.reduce(
         (acc, id, i) => (acc[id] = data[i], acc),
         {}
     ))
-    .on("data", console.log.bind(console))
+    .tee(
+        (stream) => stream
+            .on("data", (d) => process.stderr.write("\r" + hourglass.charAt(i++ % hourglass.length) + " " + i))
+            .on("end", () => console.log("\n\nEnd", res, i))
+    )
+    .on("error", (err) => console.error(err && err.stack))
+    .reduce((acc, item) => {
+        acc[item.Nazwa] = acc[item.Nazwa] || 0;
+        acc[item.Nazwa]++;
+        return acc;
+    }, {})
 ;
