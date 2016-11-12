77d251db5efb9fb586f406fc219fddfe86d17f68
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2016-11-12 21:51:03 +0100

    sample extended to a reduce method

--- samples/tests/simple.js
+++ samples/tests/simple.js
@@ -6,26 +6,29 @@ let i = 0;
 
 let columns = null;
 
-console.log();
-
 const res = request.get("http://www.wroclaw.pl/open-data/opendata/its/parkingi/parkingi.csv")
     .pipe(new StringStream())
     .split("\n")
+    // .on("data", () => console.log("K", i++))
     .parse((line) => line.split(";"))
     .filter((data) => columns === null ? (columns = data, false) : true)
     .map((data) => columns.reduce(
         (acc, id, i) => (acc[id] = data[i], acc),
         {}
     ))
-    .tee(
-        (stream) => stream
-            .on("data", (d) => process.stderr.write("\r" + hourglass.charAt(i++ % hourglass.length) + " " + i))
-            .on("end", () => console.log("\n\nEnd", res, i))
-    )
+    // .tee(
+    //     (stream) => stream
+            // .on("data", (d) => process.stderr.write("\r" + hourglass.charAt(i++ % hourglass.length) + " " + i))
+    // )
     .on("error", (err) => console.error(err && err.stack))
+    .on("end", () => console.log("\n\nEnd", res, i))
+    .map((data) => JSON.stringify(data))
     .reduce((acc, item) => {
+        console.log("Z", i++);
         acc[item.Nazwa] = acc[item.Nazwa] || 0;
         acc[item.Nazwa]++;
         return acc;
     }, {})
+    // .pipe(process.stdout)
+
 ;
