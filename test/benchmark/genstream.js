4d9e78fd45f40498eee961179362cc13081e135f
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-02-07 15:06:11 +0100

    inital benchmark

---
+++ test/benchmark/genstream.js
@@ -0,0 +1,34 @@
+const Readable = require("stream").Readable;
+const path = require('path');
+
+const Generator = function* (max) {
+    let next = 0;
+    while (next++ < max) {
+        yield next;
+    }
+};
+
+const max = process.argv[2];
+
+class GeneratedStream extends Readable {
+
+    constructor(max, options) {
+        super(options);
+        this.gen = Generator(max);
+    }
+
+    _read() {
+        const next = this.gen.next();
+
+        if (next.done) {
+            this.push(null);
+        } else {
+            this.push(JSON.stringify({value: next.value}) + "\n");
+        }
+    }
+}
+module.exports = GeneratedStream;
+
+if (!(path.relative(path.resolve(process.cwd(), process.argv[1]), path.resolve(__filename))))
+    new GeneratedStream(max || 10)
+        .pipe(process.stdout);
---
+++ test/benchmark/run.js
@@ -0,0 +1,21 @@
+const asyncServer = require("./async-reply");
+const benchmark = require("./benchmark");
+const MaxParallel = 16;
+
+const srv1 = asyncServer('test_salt_1');
+const srv2 = asyncServer('test_salt_2');
+const srv3 = asyncServer('test_salt_3');
+const srv4 = asyncServer('test_salt_4');
+
+if (process.argv.indexOf("-n") + process.argv.indexOf("--no-bench") === -2) {
+    const source = process.argv.indexOf('-') >=0 ? process.stdin : new (require("./genstream"))(10e3);
+    Promise.all([srv1, srv2, srv3, srv4]).then(
+        (ports) => benchmark(source, MaxParallel, ...ports)
+    ).catch(
+        (e) => console.error("ERROR", e && e.stack)
+    );
+} else {
+    Promise.all([srv1, srv2, srv3, srv4]).then(
+        (ports) => console.log(ports.join(","))
+    );
+}
