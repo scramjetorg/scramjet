328be9a850bf3230dc4d77df48ecf3da3a8e0fd9
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-02-16 10:35:14 +0100

    fixed flatmap and remap tap call position

--- samples/data-stream-flatmap.js
+++ samples/data-stream-flatmap.js
@@ -5,15 +5,17 @@ const DataStream = require('../').DataStream;
 exports.log = console.log.bind(console);
 
 let ref;
-DataStream.fromArray([{a: 1, b: 2}, {a: 3, b: 4}, {a: 5, b: 6}, {a: 7, b: 8}, {a: 9, b: 10}])
+DataStream.fromArray([{a: 1, b: 2, c: 11}, {a: 3, b: 4, c: 13}, {a: 5, b: 6, c: 15}, {a: 7, b: 8, c: 17}, {a: 9, b: 10, c: 19}])
     .on("end", () => exports.log("end1"))
     .debug((stream) => {
         exports.log((ref = stream)._writableState.ended);
     })
     .flatMap((item) => {
-        return Object.keys(item)
-            .map((k) => k + ':' + item[k]);
+        return Object.keys(item).map((k) => k + ':' + item[k]);
     })
+    .filter(
+        (item) => item.indexOf('b') !== 0
+    )
     .on("end", () => exports.log("end2"))
     .reduce(
         (acc, item) => (acc.push(item), acc),
