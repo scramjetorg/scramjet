d0c11162447543d5b95ae87e982d543df1556386
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-04-29 10:20:27 +0200

    fix match method

--- samples/string-stream-match.js
+++ samples/string-stream-match.js
@@ -3,10 +3,15 @@
 
 const StringStream = require('../').StringStream;
 
-const test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et\n" +
+    "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea\n" +
+    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\n" +
+    "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n" +
+    "laborum."
+;
 
 exports.log = console.log.bind(console);
 
 StringStream.fromString(test)
-    .match(/l[\w]+/g)
+    .match(/c[\w]+/gi)
     .each(exports.log);
