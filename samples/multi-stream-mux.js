613190ae25cc2dfbe838381847e5866ad6754274
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-05-04 19:40:33 +0200

    multi stream mux test

--- samples/multi-stream-mux.js
+++ samples/multi-stream-mux.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 // module: multi-stream, method: mux
 
-const MultiStream = require('../').MultiStream;
+const {MultiStream, DataStream} = require('../');
 
+exports.stream = (arr) => new MultiStream(arr);
+
+exports.test = {
+    constr(test) {
+        exports.stream([
+            DataStream.fromArray([1,3,5,7,9]),
+            DataStream.fromArray([0,2,4,6,8])
+        ])
+            .mux()
+            .reduce(
+                (acc, i) => acc + i + ",",
+                ""
+            )
+            .then(
+                (ref) => {
+                    [0,1,2,3,4,5,6,7,8,9].forEach(
+                        (num) => test.ok(ref.indexOf(num +',') >= 0, "Item " +num + " is in stream")
+                    );
+                    test.done();
+                }
+            );
+    }
+};
 
 exports.log = console.log.bind(console);
