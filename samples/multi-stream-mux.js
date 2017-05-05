cb3327f0fd61032f12eee8c029c0e5b6215ee01f
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-05-05 16:20:16 +0200

    mux test

--- samples/multi-stream-mux.js
+++ samples/multi-stream-mux.js
@@ -24,6 +24,28 @@ exports.test = {
                     test.done();
                 }
             );
+    },
+    addRemove(test) {
+        let odd = DataStream.fromArray([1,3,5,7,9]);
+        let even = DataStream.fromArray([0,2,4,6,8]);
+        let z = exports.stream();
+
+        z
+            .mux()
+            .reduce(
+                (acc, i) => acc + i + ",",
+                ""
+            )
+            .then(
+                (ref) => {
+                    [0,1,2,3,4,5,6,7,8,9].forEach(
+                        (num) => test.ok(ref.indexOf(num +',') >= 0, "Item " +num + " is in stream")
+                    );
+                    test.done();
+                }
+            );
+
+        z.add(odd).add(even);
     }
 };
 
