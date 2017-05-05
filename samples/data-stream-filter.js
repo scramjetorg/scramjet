e03aa98dd70e9c924eaa432f10c638a189b97e13
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-05-05 16:21:08 +0200

    filter test to check what happens when all items are filtered out

--- samples/data-stream-filter.js
+++ samples/data-stream-filter.js
@@ -7,23 +7,38 @@ exports.stream = () => DataStream.fromArray([1,2,3,4,5,6,7,8,9,10])
     .filter((num) => num % 2 === 0)                                             // return true for even numbers
     ;
 
-exports.test = (test) => {
+exports.test = {
+    normal: (test) => {
 
-    test.expect(2);
+        test.expect(2);
 
-    const returned = exports.stream();
+        const returned = exports.stream();
 
-    returned
-        .once("data", (num) => {
-            test.equals(num, 2, "First item must be even");
-        })
-        .toArray()
-        .then(
-            (ret) => {
-                test.equals(ret.toString(), '2,4,6,8,10', "Odd items need to be filtered out");
+        returned
+            .once("data", (num) => {
+                test.equals(num, 2, "First item must be even");
+            })
+            .toArray()
+            .then(
+                (ret) => {
+                    test.equals(ret.toString(), '2,4,6,8,10', "Odd items need to be filtered out");
+                    test.done();
+                }
+            );
+    },
+    filterAll: (test) => {
+
+        test.expect(1);
+
+        DataStream.fromArray([1,2,3,4,5])
+            .filter(() => 0)
+            .on("data", () => test.ok(false, "No data should be emitted"))
+            .on("end", () => {
+                test.ok(true, "Stream should end");
                 test.done();
-            }
-        );
+            });
+
+    }
 };
 
 exports.log = console.log.bind(console);
