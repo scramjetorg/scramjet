b0be3557da844f6e058c3c6c030ff824c1c86ea7
Michał@Vulcan <cz@fuugo.com>
2017-12-02 21:56:02 +0100

    multi threading WIP - tbe - to be execured and fixed yet

---
+++ samples/tests/data-stream-distribute.js
@@ -0,0 +1,32 @@
+const {DataStream, StreamWorker} = require('../../');
+const hash = require("string-hash");
+
+const iter = (function* () {
+    for (i = 0; i < 300; i++) {
+        yield {terms: i + 1e6};
+    }
+})();
+
+const test = DataStream.fromIterator(iter)
+//    .each((item) => console.log('pushing', item))
+    .distribute(
+        StreamWorker.fork(3),
+        (stream) => stream.map((chunk) => {
+            let {terms} = chunk;
+            let pi = 0
+            let k=1
+            while (terms != 0) {
+                terms--
+                pi = pi / 4
+                pi = pi + (1 / (   ((2*k)-1) *  (-1)*(Math.pow(-1,k))   ))
+                pi = pi * 4
+                k++
+            }
+            return {pi};
+        }),
+        item => hash(JSON.stringify(item))
+    )
+    .each((item) => console.log('got back', item))
+    .JSONStringify()
+    .pipe(process.stdout)
+;
