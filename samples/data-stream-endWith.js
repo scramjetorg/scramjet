3697685238bd4c3b4650ea1b43119bd9633e7f68
Michał@Vulcan <cz@fuugo.com>
2018-02-25 18:47:30 +0100

    fixed samples

--- samples/data-stream-endWith.js
+++ samples/data-stream-endWith.js
@@ -6,11 +6,11 @@ const DataStream = require('../').DataStream;   // eslint-disable-line
 
 exports.test = function(test) {
     test.expect(1);
-    DataStream.fromArray([1,2,3,4,5,6,7,8,9,0])
-        .slice(3,3)
+    DataStream.fromArray([1,2,3])
+        .endWith(4)
         .toArray()
         .then(arr => {
-            test.deepEqual(arr, [4,5,6], 'Shifted items should be starting with fourth element');
+            test.deepEqual(arr, [1,2,3,4], 'End with items should be added at the end');
             test.done();
         });
 };
