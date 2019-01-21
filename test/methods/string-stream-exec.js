e0b814cfb490ab02521f23d8ddf435192573fcc1
Budleigh@Vantage-buntu <cz@signicode.com>
2019-01-21 12:51:10 +0100

    Fix linux version of exec

--- test/methods/string-stream-exec.js
+++ test/methods/string-stream-exec.js
@@ -12,13 +12,13 @@ exports.log = console.log.bind(console);
 exports.test = {
     shell: {
         windows(test) {
-            if (platform() !== 'win32') {
+            if (platform() !== "win32") {
                 test.ok(true, "Only testable on win32");
                 test.done();
                 return;
             }
 
-            const path = relative('.', `./lib/test-exec.cmd`)
+            const path = relative(".", "./lib/test-exec.cmd");
             StringStream
                 .from(["a1", "b1", "c2", "c1"])
                 .append("\r\n")
@@ -32,20 +32,20 @@ exports.test = {
             ;
         },
         unix(test) {
-            if (platform() === 'win32') {
+            if (platform() === "win32") {
                 test.ok(true, "Only testable on posix platforms");
                 test.done();
                 return;
             }
 
-            const path = relative(__dirname, `./lib/test-exec.sh`)
+            const path = resolve(__dirname, "./lib/test-exec.sh");
             StringStream
                 .from(["a1", "b1", "c2", "c1"])
                 .append("\n")
                 .exec(path)
                 .split("\n")
                 .toArray()
-                .then(arr => test.deepEqual(arr, ["c2", "c1"], "Should execute the grep test"))
+                .then(arr => test.deepEqual(arr, ["c2", "c1", ""], "Should execute the grep test"))
                 .then(() => promisify(unlink)(resolve(__dirname, `${path}.did`)))
                 .catch(err => test.fail(err))
                 .then(() => test.done())
