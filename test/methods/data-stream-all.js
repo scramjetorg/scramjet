ae1d5fe2c77e9682fa22d1904012608865868c04
michal@vantage <cz@signicode.com>
2019-02-10 20:43:09 +0100

    Core update, adds methods: all and race, fixes #35

---
+++ test/methods/data-stream-all.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// module: data-stream, method: batch
+
+const {DataStream} = require("../../");
+
+exports.test = async (test) => {
+    const stream = DataStream.from([1,2,3,4]);
+
+    const arr = await (
+        stream.all([
+            x => x+1,
+            x => x+2
+        ]).toArray()
+    );
+
+    test.deepEquals(arr, [[2,3],[3,4],[4,5],[5,6]]);
+    test.done();
+};
+
+exports.log = process.env.TEST_VERBOSE === 1 ? console.log.bind(console) : () => 0;
